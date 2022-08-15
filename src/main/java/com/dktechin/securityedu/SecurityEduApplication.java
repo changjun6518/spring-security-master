938a00cbcfd8c69d88c064ad529591b8ee4f5b18
june.sh22 <june.sh22@kakaocorp.com>
2022-08-15 10:38:26 +0900

    1장: 프로젝트 구성 및 의존성

---
+++ src/main/java/com/dktechin/securityedu/SecurityEduApplication.java
@@ -0,0 +1,13 @@
+package com.dktechin.securityedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityEduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityEduApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dktechin/securityedu/SecurityEduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dktechin.securityedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityEduApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
